cf50d58987066486ceda0995a199991c6b285bc5
Alexandr <a.piatrovich@gmail.com>
2021-08-03 21:17:56 +0300

    add connection

--- src/main/java/by/http/newsportal/dao/imp/ConnectToDateBase.java
+++ src/main/java/by/http/newsportal/dao/imp/ConnectToDateBase.java
@@ -1,12 +1,12 @@
 package by.http.newsportal.dao.imp;
 
-import java.sql.*;
 import java.time.LocalDate;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.mysql.jdbc.PreparedStatement;
 
 public class ConnectToDateBase {
 	public static void main(String[] args) throws Exception{
